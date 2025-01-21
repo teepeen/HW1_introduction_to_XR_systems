c7465a17d537f823d43984bbe15faaf1fbd05457
teepeen <niemi.tommip@gmail.com>
2025-01-21 16:34:32 +0200

    Quit Button
    Still working on with button, but at least some progress

--- Assets/Quit.cs
+++ Assets/Quit.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Quit : MonoBehaviour
 {
+    public InputActionReference action;
     // Start is called before the first frame update
     void Start()
     {
